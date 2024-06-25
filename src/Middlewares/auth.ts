a4e9c6faec18320d7330535afbd4dd4c0f360dac
fcursino <fabriciocursino09@gmail.com>
2024-06-25 20:18:34 -0300

    saving user infos provided by token decode

--- src/Middlewares/auth.ts
+++ src/Middlewares/auth.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken'
+import IUser from '../Interfaces/IUser';
 
 function verifyAccessToken (req: Request, res: Response, next: NextFunction) {
   const token = req.headers['authorization'];
@@ -8,6 +9,8 @@ function verifyAccessToken (req: Request, res: Response, next: NextFunction) {
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) return res.status(403).json({ message: 'Failed to authenticate token' });
+    console.log(decoded)
+    req.body.id = decoded
     next();
   });
 }
