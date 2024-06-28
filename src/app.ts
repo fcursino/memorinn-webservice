911fd0e70a50ac063cd2421fc842049dbd4785a5
fcursino <fabriciocursino09@gmail.com>
2024-06-28 20:40:38 -0300

    including comment routes into api main router

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import bookRouter from './Routers/routerBook'
 import userRouter from './Routers/routerUser'
+import commentRouter from './Routers/routerComment'
 
 const app = express()
 app.use(express.json())
 app.use('/', bookRouter)
 app.use('/', userRouter)
+app.use('/', commentRouter)
 
 export default app
