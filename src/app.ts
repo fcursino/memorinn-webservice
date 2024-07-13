86b702180a69f5048329f446721989708290c3bd
fcursino <fabriciocursino09@gmail.com>
2024-07-13 19:10:09 -0300

    implementing cors library to handle with more complex requests

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import bookRouter from './Routers/routerBook'
 import userRouter from './Routers/routerUser'
 import commentRouter from './Routers/routerComment'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use('/', bookRouter)
 app.use('/', userRouter)
 app.use('/', commentRouter)
